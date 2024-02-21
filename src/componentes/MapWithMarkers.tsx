2ff89c3a5e0292f17a1f08839d5fe4ed5b85d524
Miguel Montania <mikemontania@gmail.com>
2024-02-21 15:42:28 -0300

    boton sincronizar

--- src/componentes/MapWithMarkers.tsx
+++ src/componentes/MapWithMarkers.tsx
@@ -1,86 +1,102 @@
 import React, { useEffect, useRef, useState } from "react";
 import MapView, { Marker } from 'react-native-maps';
 import { Reparto } from "../interfaces/Reparto.interfaces";
-import { Text, View } from "react-native";  
+import { Text, View, TouchableOpacity } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faSync } from "@fortawesome/free-solid-svg-icons";
+import { Marcador } from "../screens/DetalleRepartoScreen";
 
 interface MapWithMarkersProps {
   repartoModel?: Reparto;
   currentLocation?: { latitude: number; longitude: number };
+  markers: Marcador[];
+  getData: () => void; // Agrega el prop para el método getData
 }
 
-export const MapWithMarkers: React.FC<MapWithMarkersProps> = ({ repartoModel, currentLocation }) => {
-    const [polylines, setPolylines] = useState<any[]>([]);
-    const mapViewRef = useRef<MapView>(null);
-  const docs = repartoModel?.documento || [];
-  const markers = docs.map((documento) => ({
-    coordinate: {
-        latitude: documento.cliente.latitud || 0,
-        longitude: documento.cliente.longitud || 0,
-      },
-      title: documento.cliente.razonSocial || "",
-      description: documento.cliente.direccion || "",
-      entregado:documento.entregado
-    }));
+export const MapWithMarkers: React.FC<MapWithMarkersProps> = ({ repartoModel, currentLocation, getData, markers }) => {
+  const [polylines, setPolylines] = useState<any[]>([]);
+  const mapViewRef = useRef<MapView>(null);
+  const [markerColors, setMarkerColors] = useState<string[]>([]);
 
- 
-    let maxLat = -90;
-    let minLat = 90;
-    let maxLon = -180;
-    let minLon = 180;
+  useEffect(() => {
+    // Inicializa el estado de colores de marcadores al renderizar
+    setMarkerColors(markers.map(() => "red"));
+  }, [markers]);
 
-    markers.forEach((marker) => {
-      maxLat = Math.max(maxLat, marker.coordinate.latitude);
-      minLat = Math.min(minLat, marker.coordinate.latitude);
-      maxLon = Math.max(maxLon, marker.coordinate.longitude);
-      minLon = Math.min(minLon, marker.coordinate.longitude);
-    });
+  useEffect(() => {
+    // Actualiza los colores de los marcadores cuando cambia la propiedad entregado en los markers
+    setMarkerColors(markers.map((marker) => (marker.entregado ? "green" : "red")));
+  }, [markers]);
 
-    const midLat = (maxLat + minLat) / 2;
-    const midLon = (maxLon + minLon) / 2;
-    const latDelta = maxLat - minLat + 0.02;
-    const lonDelta = maxLon - minLon + 0.02;
+  let maxLat = -90;
+  let minLat = 90;
+  let maxLon = -180;
+  let minLon = 180;
 
-    const initialRegion = {
-      latitude: midLat,
-      longitude: midLon,
-      latitudeDelta: latDelta,
-      longitudeDelta: lonDelta,
-    };
-    useEffect(() => {
-        if (markers.length > 0 && mapViewRef.current) {
-          const coordinates = markers.map((marker) => marker.coordinate);
-          const edgePadding = { top: 70, right: 70, bottom: 70, left: 70 }; // zoom
-          mapViewRef.current.fitToCoordinates(coordinates, { edgePadding, animated: true });
-        }
-      }, [markers]);
+  markers.forEach((marker) => {
+    maxLat = Math.max(maxLat, marker.coordinate.latitude);
+    minLat = Math.min(minLat, marker.coordinate.latitude);
+    maxLon = Math.max(maxLon, marker.coordinate.longitude);
+    minLon = Math.min(minLon, marker.coordinate.longitude);
+  });
 
-    return (
-      <>
-         <MapView
-      ref={mapViewRef}
-      style={{ flex: 1 }}
-      onMapReady={() => console.log("Mapa listo")}
-    > 
-      {markers.map((marker, index) => (
-        <Marker
-          key={index}
-          coordinate={marker.coordinate}
-          title={marker.title}
-          description={marker.description}
-          pinColor={marker.entregado ? "green" : "red"}
-        />
-      ))}
-      {currentLocation && (
-        <Marker
-          coordinate={currentLocation}
-          title="Ubicación Actual"
-          pinColor="blue" // Puedes cambiar el color del marcador según tus preferencias
-        />
-      )}
-    </MapView>
-        <View style={{ position: 'absolute', top: 100, left: 50 }} /> 
-      </>
-    );
-  
+  const midLat = (maxLat + minLat) / 2;
+  const midLon = (maxLon + minLon) / 2;
+  const latDelta = maxLat - minLat + 0.02;
+  const lonDelta = maxLon - minLon + 0.02;
+
+  const initialRegion = {
+    latitude: midLat,
+    longitude: midLon,
+    latitudeDelta: latDelta,
+    longitudeDelta: lonDelta,
+  };
+
+  useEffect(() => {
+    if (markers.length > 0 && mapViewRef.current) {
+      const coordinates = markers.map((marker) => marker.coordinate);
+      const edgePadding = { top: 70, right: 70, bottom: 70, left: 70 };
+      mapViewRef.current.fitToCoordinates(coordinates, { edgePadding, animated: true });
+    }
+  }, [markers]);
+
+  return (
+    <>
+      <MapView
+        ref={mapViewRef}
+        style={{ flex: 1 }}
+        onMapReady={() => console.log("Mapa listo")}
+      >
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.coordinate}
+            title={marker.title}
+            description={marker.description}
+            pinColor={markerColors[index]} // Usar el color correspondiente
+          />
+        ))}
+        {currentLocation && (
+          <Marker
+            coordinate={currentLocation}
+            title="Ubicación Actual"
+            pinColor="blue"
+          />
+        )}
+      </MapView>
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          bottom: 20,
+          left: 20,
+          backgroundColor: 'white',
+          borderRadius: 30, // Hace que el botón sea redondeado
+          padding: 10, // Ajusta según tu preferencia
+        }}
+        onPress={getData}
+      >
+        <FontAwesomeIcon icon={faSync} size={20} color="green" />
+      </TouchableOpacity>
+    </>
+  );
 };
- 
--- src/screens/DetalleRepartoScreen.tsx
+++ src/screens/DetalleRepartoScreen.tsx
@@ -2,20 +2,35 @@ import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import { useIsFocused, useRoute } from "@react-navigation/native";
 import apiAxios from "../api/axios";
-import { Reparto } from "../interfaces/Reparto.interfaces"; 
+import { Documento, Reparto } from "../interfaces/Reparto.interfaces";
 import { MapWithMarkers } from "../componentes/MapWithMarkers";
-import Geolocation from '@react-native-community/geolocation';
+import Geolocation from "@react-native-community/geolocation";
 
 interface RouteParams {
   codReparto: number;
 }
 
+export interface Marcador {
+  coordinate: { latitude: number; longitude: number };
+  title: string;
+  description: string;
+  entregado: boolean;
+  nroComprobante:string;
+  importe: number;
+}
+
+
 export const DetalleRepartoScreen: React.FC = () => {
   const route = useRoute();
   const { codReparto } = route.params as RouteParams;
   const isFocused = useIsFocused();
+  const [markers, setMarkers] = useState<Marcador[]>([]);
   const [repartoModel, setRepartoModel] = useState<Reparto | undefined>();
-  const [currentLocation, setCurrentLocation] = useState<{ latitude: number; longitude: number }>();
+  const [currentLocation, setCurrentLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  }>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (isFocused) {
@@ -24,28 +39,52 @@ export const DetalleRepartoScreen: React.FC = () => {
   }, [isFocused]);
 
   const getData = async () => {
+    console.log("repartos");
+    setIsLoading(true);
+
     try {
       const { data } = await apiAxios.get(`/repartos/${codReparto}`);
       if (data) {
         setRepartoModel(data);
-        Geolocation.getCurrentPosition(info => {
 
-          setCurrentLocation({ latitude: info.coords.latitude, longitude: info.coords.longitude })
+        const nuevosMarcadores = data.documento.map((documento: Documento) => ({
+          coordinate: {
+            latitude: documento.cliente.latitud || 0,
+            longitude: documento.cliente.longitud || 0
+          },
+          nroComprobante: documento.venta.nroComprobante,
+          importe: documento.venta.importeTotal,
+          title: documento.cliente.razonSocial || "",
+          description: documento.cliente.direccion || "",
+          entregado: documento.entregado
+        }));
 
-        } );
+        // Actualiza los marcadores y detiene la carga
+        setMarkers(nuevosMarcadores);
+        setIsLoading(false);
 
-        
+        Geolocation.getCurrentPosition((info) => {
+          setCurrentLocation({
+            latitude: info.coords.latitude,
+            longitude: info.coords.longitude
+          });
+        });
       }
     } catch (error: any) {
-      if (error.response) console.log("Error al realizar la consulta:", error);
+      console.error("Error al realizar la consulta:", error.message);
+      setIsLoading(false);
+      // Puedes agregar una lógica para mostrar un mensaje de error al usuario
     }
   };
 
   return (
     <View style={{ flex: 1 }}>
       {/* Otras partes de tu componente */}
-      <MapWithMarkers repartoModel={repartoModel} currentLocation={currentLocation} />
+      {isLoading ? (
+        <Text>Cargando...</Text>
+      ) : (
+        <MapWithMarkers repartoModel={repartoModel} currentLocation={currentLocation} getData={getData} markers={markers} />
+      )}
     </View>
   );
 };
- 
